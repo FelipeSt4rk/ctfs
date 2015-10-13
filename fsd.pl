bb21b822bf5a141508019abaf650577eeaad7d03
HeitorG <contato.htr@hotmail.com>
2015-10-13 11:49:00 -0300

    bugs removidos

--- fsd.pl
+++ fsd.pl
@@ -2,85 +2,92 @@
 
 #### OPEN MODULOS ####
 use strict;
+use Switch;
 use warnings;
-use Term::ANSIColor;
 use IO::Socket::INET;
 #### /END MODULOS ####
 
 #### OPEN VARIAVEIS ####
 my $result;
-my $domain = $ARGV[0];
+my $option = $ARGV[0];
+my $domain = $ARGV[1];
 #### /END VARIAVEIS ####
 
 sub help {
-	print color("bold"),"\nYou can use the FindSubDomain in 3 ways:
-	\rperl fsd.pl github.com
-	\rperl fsd.pl github.com --save result.txt
-	\rperl fsd.pl github.com -s result.txt\n\n",color("reset");
+	print "\n\tCOMMAND \t FUNCTION
+	-u --url         Setting target.
+	-s --save        Saving result.\n\n
+	Examples:\n
+	perl fsd.pl -u google.com
+	perl fsd.pl --url google.com
+	perl fsd.pl -u google.com -s result.txt
+	perl fsd.pl -u google.com --save result.txt\n\n";
 	exit;
 }
 
-if (!$domain) {
-	return help();
+sub error {
+	print "\nERROR: It is not possible to open the file 'subs.txt'\n\n";
+	exit; 
 }
 
-if ( ($domain eq "-h") || ($domain eq "--help") ) {
-	return help();
-}
+sub find {
+	if (!$domain) { help(); }
 
-if ($ARGV[1]) {
+	if ($ARGV[2]) {
 
-	if ( ($ARGV[1] ne "-s") && ($ARGV[1] ne "--save") ) {
-		print color("bold red"),"\n[+] WARNING: option incompatible.Use '-'.\n",color("reset");
-		exit; # Finalizando Script
-	}
-
-	my $name = $ARGV[2];
-
-	if (!$name) {
-		print color("bold red"),"\nERROR: For this option set the file name.
-		\rFollor the example: perl fsd.pl > result.txt\n\n",color("reset");
-		exit; # Finalizando Script
-	}
-
-	open ( $result, ">$name");
-}
-
-#### AJUSTES
-$domain =~ s/https:\/\/// || $domain =~ s/http:\/\/// || $domain =~ s/www.// ;
-
-sub error { #### SUB ROTINA FOR ERROR
-	print color("bold red"),"\nERROR: It is not possible to open the file 'subs.txt'\n\n",color("reset");
-	exit; # Finalizando Script
-}
-
-open (my $subs,"<subs.txt") or die error(); # Abrindo Lista de sub dominios
-
-while (<$subs>) { # Criando Loop
-	chomp ($_);
-
-	my $find = $_ . $domain; # Concatenando alvo + wordlist
-
-	my $socket = IO::Socket::INET -> new ( # Criando socket
-		PeerAddr => $find, # Definindo alvo
-		PeerPort => "80",  # Definindo Porta
-		Proto    => "tcp", # Definindo Protocolo
-		Reuse    => "1",   # Reusando o socket
-		Timeout  => "5");  # Tempo maximo de resposta
-
-	if ($socket) { # Imprimindo resposta
-		my $IpAddr = inet_ntoa (scalar gethostbyname($find)); # Encontrando IP
-		print color("bold green"),"\n$find $IpAddr\n",color("reset");
-
-		if ($ARGV[2]) {
-			my $IpAddr = inet_ntoa (scalar gethostbyname($find)); # Encontrando IP
-			print $result "\n$find $IpAddr\n";
+		if ( ($ARGV[2] ne "-s") && ($ARGV[2] ne "--save") ) {
+			print "\n[+] WARNING: option incompatible. Use '--save' or '-s'.
+			\rFollow the example: perl fsd.pl --url google.com --save result.txt\n";
+			exit; 
 		}
 
+		my $name = $ARGV[3];
+
+		if (!$name) {
+			print "\nERROR: For this option set the file name.
+			\rFollow the example: perl fsd.pl --url google.com --save result.txt\n\n";
+			exit; 
+		}
+
+		open ( $result, ">$name");
 	}
-	
+
+	#### Regex para remoer https - http - www. se necessario
+	$domain =~ s/https:\/\/// || $domain =~ s/http:\/\/// || $domain =~ s/www.// ;
+
+	open ( my $subs,"<subs.txt") or die error(); # Abrindo Lista de sub dominios
+
+	while (<$subs>) { # Criando Loop
+		chomp ($_);
+
+		my $find = $_ . $domain; # Concatenando alvo + wordlist
+
+		my $socket = IO::Socket::INET -> new ( # Criando socket
+			PeerAddr => $find, # Definindo alvo
+			PeerPort => "80",  # Definindo Porta
+			Proto    => "tcp", # Definindo Protocolo
+			Reuse    => "1",   # Reusando o socket
+			Timeout  => "5");  # Tempo maximo de resposta
+
+		if ($socket) {
+
+			my $IpAddr = inet_ntoa (scalar gethostbyname($find));
+			print "\n$find $IpAddr\n";
+
+			if ($ARGV[2]) {
+				print $result "\n$find $IpAddr\n";
+			}
+		}
+	}
+
+	close $result;
+	close $subs;
+	exit;
 }
 
-close $result;
-close $subs; # Fechando wordlist
-exit; # Finalizando Script
+switch ($option) {
+	case "--url" { find(); }
+	case "-u"    { find(); }
+	case ""      { help(); }
+	else         { help(); }
+}
